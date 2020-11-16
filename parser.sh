88e720599a3496e0d5d34b18dbe0d23630fd7cfb
Yoav Vainrich <yoavain@gmail.com>
2020-11-16 15:23:14 +0200

    * Parser script that accept an input file * input sample file * Initial git actions file

--- parser.sh
+++ parser.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/bash
 
 # Cleanup
-rm ./*.sorted.txt ./*.unsorted.txt 2> /dev/null
+rm ./*.raw.txt 2> /dev/null
 
 while read -r inputUrl targetFile; do
   echo Parsing "${inputUrl}" into "${targetFile}"
-  unsortedFile="${targetFile}.unsorted.txt"
-  sortedFile="${targetFile}.sorted.txt"
+  rawFile="${targetFile}.raw.txt"
 
   # Allowing multiple input URLs to the same target
-  curl -s -L "${inputUrl}" >> "${unsortedFile}"
+  curl -s -L "${inputUrl}" >> "${rawFile}"
 
-  sort -u "${unsortedFile}" | grep ^\|\|.*\^$ | grep -v \/ > "${sortedFile}"
-  sed 's/[\*|^]//g' < "${sortedFile}" >> "${targetFile}"
+  sort -u "${rawFile}" | grep '^||.*\^\$.*' | sed 's/[\*|^]//g' | sed 's/\$.*//g' | grep -v "\/" > "${targetFile}"
 done < input.txt
 
 # Cleanup
-rm ./*.sorted.txt ./*.unsorted.txt 2> /dev/null
+rm ./*.raw.txt 2> /dev/null
