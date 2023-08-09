c4a79f0dd84e6d5a47408a7d0bb2a157c62faf87
Yoav Vainrich <yoavain@gmail.com>
2023-08-09 07:35:06 +0300

    * NodeJS 18.x in GitHub actions * Update dependencies

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+    clearMocks: true,
+    transform: {
+        "^.+\\.ts$": "ts-jest"
+    },
+    testRegex: "(test|test-generated)/.*.test.ts$",
+    moduleFileExtensions: ["ts", "js", "json", "node"],
+    moduleNameMapper: {
+        "^~src/(.*)": "<rootDir>/src/$1",
+        "^~test/(.*)": "<rootDir>/test/$1",
+        "^~resources/(.*)": "<rootDir>/resources/$1"
+    },
+    verbose: true,
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageReporters: [
+        "text",
+        "text-summary",
+        "json",
+        "lcov",
+        "clover"
+    ],
+    collectCoverageFrom: ["src/**/*.ts"]
+};
+
+export default config;
